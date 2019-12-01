a1728e24de098b4cf16c9102860e0abf1a9028a6
NAD <123wins@mail.ru>
2019-12-01 22:11:42 +0300

    01.12.2019

--- component/config.py
+++ component/config.py
@@ -1,5 +1,5 @@
 weather_list = ['94d8b74c4d0c302754e1af1f42419289','Minsk']
-bot_api = '972269113:AAE0G6hsCJt_g8mIHEwW5_LFcTe5ifOSRSg'
+bot_api = ''
 weather_api = 'http://api.openweathermap.org/data/2.5/weather?appid={}&q={}&units=metric'.format(weather_list[0], weather_list[1])
 courses_api = 'http://www.nbrb.by/API/ExRates/Rates?Periodicity=0'
 movies_api = 'https://afisha.tut.by/film/'
